7fa240e3e4e341a1129f75ef7b402c8f221067e7
Kuldeep3330 <upsainian3330@gmail.com>
2026-02-08 22:48:34 +0530

    db connection established

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 import express from "express";
-
+import mongoose from "mongoose";
 const app = express();
 const PORT = 4000;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/ingestion_db")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB error:", err));
 
 app.get("/", (req, res) => {
   res.send("hello world");
